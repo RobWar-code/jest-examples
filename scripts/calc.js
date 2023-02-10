38614f1d077f1965866633fc84bda371c237ab83
RobWar-code <rowaniar@aol.com>
2023-02-10 18:41:26 +0000

    Add html/javascript mock test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,5 @@
-function addition() {
-    return 42;
+function addition(a, b) {
+    return a + b;
 }
 
 // Test module link
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,9 @@ describe("Calculator", () => {
         test("Should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
         });
+        test("Should return 73 for 41 + 32", () => {
+            expect(addition(41, 32)).toBe(73);
+        });
     });
     describe("Subtraction function", () => {
 
